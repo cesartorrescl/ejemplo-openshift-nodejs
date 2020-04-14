523687afb3e544ae0ede52f304c3c07f84d92f6d
César Torres <cesartorres.cl@gmail.com>
2020-04-14 19:29:47 -0400

    ejemplo de servicio en puerto 8080

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("archivo de prueba")
+var http = require('http');
+ 
+// Configurar una respuesta HTTP para todas las peticiones
+function onRequest(request, response) {
+  console.log("Peticion Recibida.");
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write("Aplicación ejemplo");
+  response.end();
+}
+ 
+var server = http.createServer(onRequest);
+ 
+// Escuchar al puerto 8080
+server.listen(8080);
+console.log("Servidor funcionando en http://localhost:8080/");
